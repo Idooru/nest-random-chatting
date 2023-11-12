4062a7dd352c4135d5e091cf28a01a54cfa3c69f
idooru <shere1765@gmail.com>
2023-11-12 20:10:53 +0900

    HTTP vs Socket

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,17 @@
-alert('hello world!');
+const socket = io('/');
+const getElementById = (id) => document.getElementById(id) || null;
+
+//* get DOM Element
+const helloStrangerElement = getElementById('hello_stranger');
+const chattingBoxElement = getElementById('chatting_box');
+const formElement = getElementById('chat_form');
+
+function helloUser() {
+  return prompt('What is your name?');
+}
+
+function init() {
+  helloUser();
+}
+
+init();
