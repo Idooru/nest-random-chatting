a9b00b27bd919da1a3ecade7586b6f7d0e317c95
idooru <shere1765@gmail.com>
2023-11-12 21:13:59 +0900

    네임스페이스의 이해와 Gateway 생명주기

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -2,17 +2,42 @@ import { Socket } from 'socket.io';
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
 
-@WebSocketGateway()
-export class ChatsGateway {
+@WebSocketGateway({ namespace: '/chattings' })
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('chat');
+
+  constructor() {
+    this.logger.log('constructor');
+  }
+
+  afterInit(): any {
+    this.logger.log('init');
+  }
+
+  handleConnection(@ConnectedSocket() socket: Socket): any {
+    this.logger.log(`connect: ${socket.id} ${socket.nsp.name}`);
+  }
+
+  handleDisconnect(@ConnectedSocket() socket: Socket): any {
+    this.logger.log(`disconnect: ${socket.id} ${socket.nsp.name}`);
+  }
+
   @SubscribeMessage('new_user')
   handleNewUser(
     @MessageBody() username: string,
     @ConnectedSocket() socket: Socket,
   ): string {
+    this.logger.log(`${username} is entered`);
     console.log(username);
     console.log(socket.id);
     socket.emit('hello_user', 'hello ' + username);
