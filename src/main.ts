7703a563de4d1351e5af2f70695e819f53d18195
idooru <shere1765@gmail.com>
2023-11-13 22:58:10 +0900

    배포 준비

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -12,6 +12,10 @@ async function bootstrap() {
   app.setViewEngine('hbs');
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT, () => {
+    new Logger('NestApplication').log(
+      `Server is running on http://${process.env.HOST}:${process.env.PORT}`,
+    );
+  });
 }
 bootstrap();
